4f1591ead6f2171d41e8565647a3892fb75132a4
AmbervanderPol <91132347+AmbervanderPol@users.noreply.github.com>
2021-11-04 16:18:18 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,2 +1,15 @@
+import pandas as pd
+import requests
+import json
+import cbsodata
+import geopandas as gpd
+from shapely.geometry.multipolygon import MultiPolygon
+import shapely.wkt
+import folium
+import numpy as np
+import plotly.graph_objects as go
 import streamlit as st
-st.head(' Eindopdracht ')
+
+inkomen = pd.read_csv('Inkomen_per_regio.csv', delimiter = ';')
+aantal_mensen = pd.read_csv('Aantal_mensen_per_regio.csv', delimiter=';')
+opleiding = pd.read_csv('Opleiding_per_regio.csv', delimiter =';')
