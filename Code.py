6b73cb1107c55e1c194609877654269721fb9776
AmbervanderPol <91132347+AmbervanderPol@users.noreply.github.com>
2021-11-10 21:12:56 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -26,6 +26,78 @@ st.sidebar.write('''Dashboard is gemaakt door Amber van der Pol (500803136) en O
 df = pd.read_csv('Opleiding_inkomen.csv')
 
 
+col1, col2 = st.columns(2)
+
+with col1: 
+  st.markdown('''**Rijksdienst voor het Wegverkeer (RDW)**  
+  De data over auto’s en hun brandstoftypes komt van het RDW. Deze twee datasets zijn als csv binnengehaald, samengevoegd en vervolgens **geïnspecteerd**.  
+  
+  •	In de datasets komen geen **duplicates** voor.  
+  •	**Missende waardes** zijn niet meegenomen in de visualisatie van de data.  
+  •	Bij het inspecteren van deze data zijn geen nieuwe variabelen gegenereerd maar is er wel een **nieuwe dataset** gecreëerd door twee datasets te mergen.  
+  
+  bron: https://opendata.rdw.nl/Voertuigen/Open-Data-RDW-Gekentekende_voertuigen_brandstof/8ys7-d773 en https://opendata.rdw.nl/Voertuigen/Open-Data-RDW-Gekentekende_voertuigen/m9d7-ebf2''')
+with col2: 
+  st.markdown('''**Kaart**)
+  # Kaart
+  coördinaten = [[53.217503, 6.566677], [53.200736, 5.795717], [52.878580, 6.646088], [52.441316, 6.398667], [52.435788, 5.453631],[52.056569, 6.028022], [52.091001, 5.117947], [52.377173, 4.902515], [51.916845, 4.482816], [51.495115, 3.842055], [51.592708, 5.129947], [51.187293, 5.999778]]
+  df['middelste_punt'] = coördinaten
+  m = folium.Map(location=[52.0893191, 5.1101691], zoom_start = 7, tiles="cartodbpositron")
+  Mbo = folium.FeatureGroup(name="Mbo", show = False).add_to(m)
+  Hbo = folium.FeatureGroup(name="Hbo", show = False).add_to(m)
+  Wo = folium.FeatureGroup(name="Wo", show = False).add_to(m)
+  Inwoners = folium.FeatureGroup(name="Inwoners", show = True).add_to(m)
+  Eenpersoonshuishouden = folium.FeatureGroup(name="Eenpersoonshuishouden", show = False).add_to(m)
+  Particulierehuishoudens = folium.FeatureGroup(name="Particulierehuishoudens", show = False).add_to(m)
+  Paarkinderen =folium.FeatureGroup(name="Paarkinderen", show = False).add_to(m)
+  x=0
+  for row in df.iterrows(): 
+    row_values = row[1]
+    Inwoners.add_child(folium.vector_layers.Circle(radius = row_values['pop_radius'], location = row_values['middelste_punt'], fill = True, color='yellow').add_to(m))
+    Mbo.add_child(folium.vector_layers.Circle(radius = row_values['Mbo_radius'], location = row_values['middelste_punt'], fill = True, color='red').add_to(m))
+    Hbo.add_child(folium.vector_layers.Circle(radius = row_values['Hbo_radius'], location = row_values['middelste_punt'], fill = True, color='blue').add_to(m))
+    Wo.add_child(folium.vector_layers.Circle(radius = row_values['Wo_radius'], location = row_values['middelste_punt'], fill = True, color='purple').add_to(m))
+    Eenpersoonshuishouden.add_child(folium.vector_layers.Circle(radius = row_values['eenpersoonshuishouden_radius'], location = row_values['middelste_punt'], fill = True, color='dimgrey').add_to(m))
+    Particulierehuishoudens.add_child(folium.vector_layers.Circle(radius = row_values['particulierehuishouden_radius'], location = row_values['middelste_punt'], fill = True, color='lightcoral').add_to(m))
+    Paarkinderen.add_child(folium.vector_layers.Circle(radius = row_values['paarmetkinderen_radius'], location = row_values['middelste_punt'], fill = True, color='skyblue').add_to(m))
+    folium.LayerControl().add_to(m)
+    # # folium.TileLayer('cartodbpositron', name='Onderwijs niveau').add_to(LayerControl)
+    # display(m)
+    # st.map(m)
+    folium_static(m, width = 1200, height = 700)
+
+#col1, col2 = st.columns(2)
+#with col1: 
+## Boxplot
+### Inkomen
+fig1 = go.Figure()
+fig1.add_trace(go.Box(y=df['particuliere huishoudens excl. studenten, gem. besteedbaar inkomen'], name = 'Particuliere huishoudens excl. studenten' , marker_color='rgb(127, 60, 141)'))
+fig1.add_trace(go.Box(y=df['eenpersoons- huishouden, gem. besteedbaar inkomen'], name = 'Eenpersoons- huishouden', marker_color='rgb(17, 165, 121)'))
+fig1.add_trace(go.Box(y=df['eenouder- gezin, gem. besteedbaar inkomen'], name = 'Eenouder- gezin', marker_color='rgb(57, 105, 172)'))
+fig1.add_trace(go.Box(y=df['paar zonder kind, gem. besteedbaar inkomen'], name = 'Paar zonder kind', marker_color='rgb(242, 183, 1)'))
+fig1.add_trace(go.Box(y=df['paar met kinderen, gem. besteedbaar inkomen'], name = 'Paar met kinderen', marker_color='rgb(231, 63, 116)'))
+fig1.add_trace(go.Box(y=df['inkomen als werknemer, gem. besteedbaar inkomen'], name = 'Inkomen als werknemer', marker_color='rgb(128, 186, 90)'))
+fig1.update_layout(title_text = 'Gemiddeld besteedbaar inkomen per soort huishouden', legend_title='Verschillende huishoudens:')
+fig1.update_yaxes(title = 'Gemiddeld besteedbaar inkomen')
+fig1.show()
+
+### Opleiding
+fig2 = go.Figure()
+fig2.add_trace(go.Box(y=df['Totaal voortgezet onderwijs (vo)'], name = 'Totaal voortgezet onderwijs (vo)', marker_color='rgb(127, 60, 141)' ))
+fig2.add_trace(go.Box(y=df['Vavo'], name = 'Vavo', marker_color='rgb(165, 170, 153)'))
+fig2.add_trace(go.Box(y=df['Totaal mbo (incl. extranei)'], name = 'Totaal mbo (incl. extranei)', marker_color='rgb(17, 165, 121)'))
+fig2.add_trace(go.Box(y=df['Hbo-associate degree'], name = 'Hbo-associate degree', marker_color='rgb(57, 105, 172)'))
+fig2.add_trace(go.Box(y=df['Hbo-bachelor'], name = 'Hbo-bachelor', marker_color='rgb(242, 183, 1)'))
+fig2.add_trace(go.Box(y=df['Hbo-master/vervolgopleiding'], name = 'Hbo-master/vervolgopleiding', marker_color='rgb(231, 63, 116)'))
+fig2.add_trace(go.Box(y=df['Wo-bachelor'], name = 'Wo-bachelor', marker_color='rgb(128, 186, 90)'))
+fig2.add_trace(go.Box(y=df['Wo-master'],  name = 'Wo-master', marker_color='rgb(230, 131, 16)'))
+fig2.add_trace(go.Box(y=df['Wo-doctoraal'],  name = 'Wo-doctoraal', marker_color='rgb(0, 134, 149)'))
+fig2.add_trace(go.Box(y=df['Wo-vervolgopleiding'], name = 'Wo-vervolgopleiding', marker_color='rgb(207, 28, 144)'))
+fig2.update_layout(title_text = 'Aantal afgestudeerde per opleidingssoort', legend_title='Opleiding:')
+fig2.update_traces(width=1)
+fig2.update_yaxes(title = 'Aantal afgestudeerde')
+fig2.show()
+
 # Subscatter plot
 fig = make_subplots(rows=3, cols=3, subplot_titles=("Mbo scholieren","Hbo studenten", "Wo studenten"))
 fig.add_trace(
@@ -65,87 +137,6 @@ fig.update_layout(height=800, width=900, title_text="Inkomen en opleiding",showl
 fig.show()
 st.plotly_chart(fig)
 
-# Kaart
-coördinaten = [[53.217503, 6.566677], [53.200736, 5.795717], [52.878580, 6.646088], [52.441316, 6.398667], [52.435788, 5.453631],[52.056569, 6.028022], [52.091001, 5.117947], [52.377173, 4.902515], [51.916845, 4.482816], [51.495115, 3.842055], [51.592708, 5.129947], [51.187293, 5.999778]]
-df['middelste_punt'] = coördinaten
-m = folium.Map(location=[52.0893191, 5.1101691], zoom_start = 7, tiles="cartodbpositron")
-Mbo = folium.FeatureGroup(name="Mbo", show = False).add_to(m)
-Hbo = folium.FeatureGroup(name="Hbo", show = False).add_to(m)
-Wo = folium.FeatureGroup(name="Wo", show = False).add_to(m)
-Inwoners = folium.FeatureGroup(name="Inwoners", show = True).add_to(m)
-Eenpersoonshuishouden = folium.FeatureGroup(name="Eenpersoonshuishouden", show = False).add_to(m)
-Particulierehuishoudens = folium.FeatureGroup(name="Particulierehuishoudens", show = False).add_to(m)
-Paarkinderen =folium.FeatureGroup(name="Paarkinderen", show = False).add_to(m)
-x=0
-for row in df.iterrows(): 
-    row_values = row[1]
-    Inwoners.add_child(folium.vector_layers.Circle(radius = row_values['pop_radius'], location = row_values['middelste_punt'], 
-                                           fill = True, color='yellow').add_to(m))
-    Mbo.add_child(folium.vector_layers.Circle(radius = row_values['Mbo_radius'], location = row_values['middelste_punt'], 
-                                           fill = True, color='red').add_to(m))
-    Hbo.add_child(folium.vector_layers.Circle(radius = row_values['Hbo_radius'], location = row_values['middelste_punt'], 
-                                          fill = True, color='blue').add_to(m))
-    Wo.add_child(folium.vector_layers.Circle(radius = row_values['Wo_radius'], location = row_values['middelste_punt'], 
-                                          fill = True, color='purple').add_to(m))
-    Eenpersoonshuishouden.add_child(folium.vector_layers.Circle(radius = row_values['eenpersoonshuishouden_radius'], location = row_values['middelste_punt'], 
-                                          fill = True, color='dimgrey').add_to(m))
-    Particulierehuishoudens.add_child(folium.vector_layers.Circle(radius = row_values['particulierehuishouden_radius'], location = row_values['middelste_punt'], 
-                                          fill = True, color='lightcoral').add_to(m))
-    Paarkinderen.add_child(folium.vector_layers.Circle(radius = row_values['paarmetkinderen_radius'], location = row_values['middelste_punt'], 
-                                          fill = True, color='skyblue').add_to(m))
-folium.LayerControl().add_to(m)
-# # folium.TileLayer('cartodbpositron', name='Onderwijs niveau').add_to(LayerControl)
-# display(m)
-# st.map(m)
-folium_static(m, width = 1200, height = 700)
-
-## Boxplot
-### Inkomen
-fig1 = go.Figure()
-fig1.add_trace(go.Box(y=df['particuliere huishoudens excl. studenten, gem. besteedbaar inkomen'],
-                    name = 'Particuliere huishoudens excl. studenten' , marker_color='rgb(127, 60, 141)'))
-fig1.add_trace(go.Box(y=df['eenpersoons- huishouden, gem. besteedbaar inkomen'],
-                    name = 'Eenpersoons- huishouden', marker_color='rgb(17, 165, 121)'))
-fig1.add_trace(go.Box(y=df['eenouder- gezin, gem. besteedbaar inkomen'],
-                    name = 'Eenouder- gezin', marker_color='rgb(57, 105, 172)'))
-fig1.add_trace(go.Box(y=df['paar zonder kind, gem. besteedbaar inkomen'],
-                    name = 'Paar zonder kind', marker_color='rgb(242, 183, 1)'))
-fig1.add_trace(go.Box(y=df['paar met kinderen, gem. besteedbaar inkomen'],
-                    name = 'Paar met kinderen', marker_color='rgb(231, 63, 116)'))
-fig1.add_trace(go.Box(y=df['inkomen als werknemer, gem. besteedbaar inkomen'],
-                    name = 'Inkomen als werknemer', marker_color='rgb(128, 186, 90)'))
-fig1.update_layout(title_text = 'Gemiddeld besteedbaar inkomen per soort huishouden',
-                 legend_title='Verschillende huishoudens:')
-fig1.update_yaxes(title = 'Gemiddeld besteedbaar inkomen')
-fig1.show()
-
-### Opleiding
-fig2 = go.Figure()
-fig2.add_trace(go.Box(y=df['Totaal voortgezet onderwijs (vo)'],
-                    name = 'Totaal voortgezet onderwijs (vo)', marker_color='rgb(127, 60, 141)' ))
-fig2.add_trace(go.Box(y=df['Vavo'],
-                    name = 'Vavo', marker_color='rgb(165, 170, 153)'))
-fig2.add_trace(go.Box(y=df['Totaal mbo (incl. extranei)'],
-                    name = 'Totaal mbo (incl. extranei)', marker_color='rgb(17, 165, 121)'))
-fig2.add_trace(go.Box(y=df['Hbo-associate degree'],
-                    name = 'Hbo-associate degree', marker_color='rgb(57, 105, 172)'))
-fig2.add_trace(go.Box(y=df['Hbo-bachelor'],
-                    name = 'Hbo-bachelor', marker_color='rgb(242, 183, 1)'))
-fig2.add_trace(go.Box(y=df['Hbo-master/vervolgopleiding'],
-                    name = 'Hbo-master/vervolgopleiding', marker_color='rgb(231, 63, 116)'))
-fig2.add_trace(go.Box(y=df['Wo-bachelor'],
-                    name = 'Wo-bachelor', marker_color='rgb(128, 186, 90)'))
-fig2.add_trace(go.Box(y=df['Wo-master'],
-                    name = 'Wo-master', marker_color='rgb(230, 131, 16)'))
-fig2.add_trace(go.Box(y=df['Wo-doctoraal'],
-                    name = 'Wo-doctoraal', marker_color='rgb(0, 134, 149)'))
-fig2.add_trace(go.Box(y=df['Wo-vervolgopleiding'],
-                    name = 'Wo-vervolgopleiding', marker_color='rgb(207, 28, 144)'))
-fig2.update_layout(title_text = 'Aantal afgestudeerde per opleidingssoort',
-                 legend_title='Opleiding:')
-fig2.update_traces(width=1)
-fig2.update_yaxes(title = 'Aantal afgestudeerde')
-fig2.show()
 
 # Lineaire regressie
 
