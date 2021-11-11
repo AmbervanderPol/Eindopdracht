b5de5a30a5e6e2208e10c9ed3f92b75f7620b89a
AmbervanderPol <91132347+AmbervanderPol@users.noreply.github.com>
2021-11-11 10:40:24 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -31,11 +31,9 @@ col1, col2 = st.columns(2)
 
 with col1: 
   st.markdown('''**Inkomen en opleiding**  
-  Allemaal informatie over de inkomen en opleiding.  
-  
-  •	Wat gaan we zien?
-  •	Wat is opvallend?
-  •	Andere leuke informatie''')
+  Gelijkheid is een van onze belangrijkste normen en waarden in Nederland daarom onderzoeken we de kans-gelijkheid in het onderwijs. 
+  Hiervoor hebben we de datasets van het inkomen per provincie en het gediplomeerden per onderwijsniveau per provincie van het CBS gehaald. 
+  De twee datasets hebben we aan elkaar gekoppeld per provincie. Om zo te onderzoeken of rijker zijn ook betekent dat je kansrijker bent.''')
 with col2: 
   st.markdown('''**Kaart**''')
   # Kaart
